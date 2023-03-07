8596b5615269d063e489b236037d9a9ad4c3bb11
Kamil Sikora <avler455@gmail.com>
2023-03-07 21:40:58 +0100

    Start-03

---
+++ src/components/Rps.tsx
@@ -0,0 +1,33 @@
+
+export default function Rps() {
+    return(
+        <div className='conteiner'>
+        <div className="cont-header">
+            <div>
+                <p className="header-text">ROCK</p>
+                <p className="header-text">PAPER</p>
+                <p className="header-text">SCISORS</p>
+            </div>
+            <div className="cont-header-score">
+                <p className="score-text">SCORE</p>
+                <p className="score">12</p>
+            </div>
+        </div>
+        <div className="cont-main">
+            <div className="cont-main-elem-paper">
+                <img src="src\assets\images\icon-paper.svg" alt="paper" />
+            </div>
+            <div className="cont-main-elem-scissors">
+                <img src="src\assets\images\icon-scissors.svg" alt="paper" />
+            </div>
+            <div className="cont-main-elem-rock">
+                <img src="src\assets\images\icon-rock.svg" alt="paper" />
+            </div>
+        </div>
+        <div className="cont-botom">
+            <button className="btn">START</button>
+            <button className="btn">RULES</button>
+        </div>
+    </div>
+    )
+}
