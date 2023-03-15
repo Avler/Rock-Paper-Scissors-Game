f44de3165d55e51a105d436443583b37fe04d05c
Kamil Sikora <avler455@gmail.com>
2023-03-15 16:53:15 +0100

    result-01

---
+++ src/components/Rpsresult.tsx
@@ -0,0 +1,9 @@
+
+export default function RpsResult () {
+    return (
+        <div className="cont-match">
+            <div className="">1</div>
+            <div className="">2</div>
+        </div>
+    )
+}
