a9ba4f57c1eccbaa38c86fec94519b00bc201d9e
Kamil Sikora <avler455@gmail.com>
2023-03-16 14:50:26 +0100

    sm-game

--- src/components/Rpsresult.tsx
+++ src/components/Rpsresult.tsx
@@ -1,3 +1,5 @@
+import { useState , useEffect } from "react";
+
 interface dataElm {
     name: string ;
     id: number | string ;
@@ -8,9 +10,29 @@ interface dataElm {
 }
 
 export default function RpsResult (props:any) {
+    const [result , setResult] = useState("")
     const data = props.data
     let objclass:string
-  const resultelm =  data.map( (elem:dataElm) => {
+    
+    const selectedElm = data.filter((elm:dataElm) => elm.selected)
+    const objects = selectedElm.length === 1 ? [selectedElm[0] , selectedElm[0]] : selectedElm
+
+    useEffect(() => {
+        if (objects.length === 2) {
+            if ((objects[0].name === "rock" && objects[1].name === "scissors") || 
+            (objects[0].name === "paper" && objects[1].name === "rock") || 
+            (objects[0].name === "scissors" && objects[1].name === "paper") 
+            ) {
+                setResult("YOU WIN")
+            } else if ( objects[0].name === objects[1].name) {
+                setResult("TIE GAME ")
+            } else {
+                setResult("YOU LOSE")
+            }
+        }
+    }, [objects])
+
+    const resultelm =  objects.map( (elem:dataElm ) => {
     
         if(elem.name === "paper") {
            objclass  = "cont-main-elem-paper-selected"
@@ -19,12 +41,18 @@ export default function RpsResult (props:any) {
         } else {
             objclass = "cont-main-elem-rock-selected"
         }
-    return elem.selected ?  <div className={objclass}><img src={elem.img} alt={elem.name}/></div> : null
+
+    return   <div className={objclass}><img src={elem.img} alt={elem.name}/></div> 
+    
   }  
   )
     return (
+        <>
         <div className="cont-match">
-            {resultelm}
+            {resultelm}  
         </div>
+        <p className="resoult-text">{result}</p>
+        </>
+        
     )
 }
